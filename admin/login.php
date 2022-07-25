a05abe4ffb3020eb21e789b12190d36e3350893c
TitSober <45947463+TitSober@users.noreply.github.com>
2022-07-25 11:54:01 +0200

    Add files via upload

---
+++ admin/login.php
@@ -0,0 +1,137 @@
+<?php
+include('../backend/db_connect.php')
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/font.css">
+    <script src="js/bootstrap.js"></script>
+    <script src="https://kit.fontawesome.com/ef06bd2c19.js" crossorigin="anonymous"></script>
+    
+
+
+    <title>F1</title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><h1>SF1L</h1></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+      <li class="nav-item dropdown ">
+          <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            SEZONE
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+
+
+          <li class="nav-item dropdown dropdown-submenu ">
+            <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdownMenuLink1" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                SEZONE
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink1">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+
+
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+
+
+        <li class="nav-item dropdown ">
+          <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            KOMISIJSKA SOBA
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active me-3" href="#">KOLEDAR</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active me-3" href="#">STATISTIKA</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active me-3" href="#">PRIJAVA</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active me-3" href="#">O NAS</a>
+        </li>
+        <li class="nav-item">
+          <a class=" nav-link me-3 active" href="admin/login.php" ><i class="fas fa-user-alt" aria-hidden="true"></i></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- Main body of page other is static -->
+<div class="container  text-center me-6 ms-6">
+    <form action="../backend/login-admin.php" class="border border-dark rounded p-3" method="post">
+    <div class="row">
+        <div class="col"><h1 class="text-dark fb-bold">Login</h1></div>
+    </div>
+    <div class="row">
+        <div class="col"><label for="username">Username|Email</label></div>
+    </div>
+    <div class="row">
+        <div class="col-3 m-auto"><input type="text" id="username" name="username" class="form-control"></div>
+    </div>
+    <div class="row">
+        <div class="col"><label for="password">Password</label></div>
+    </div>
+    <div class="row">
+        <div class="col-3 m-auto"><input type="password" name="password" id="password" class="form-control"></div>
+    </div>
+    <div class="row">
+        <div class="col"><button class="btn btn-primary" type="submit">SUBMIT</button></div>
+    </div>
+    </form>
+</div>
+
+
+
+
+
+<footer id="footer" class="mt-auto py-3 footer bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="text-center p-4">
+    © 2022 Copyright:
+    <a class="text-reset fw-bold" href="http://titsober.com/">titsober.com</a>
+                </div>
+            </div>
+            <div class="col">
+                <div class="text-center p-4">
+                    <a href="#">Ikonca</a>
+                    <a href="#">Ikonca</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    
+</footer>
+</body>
+</html>
