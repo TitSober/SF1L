ecb6679ca8576525e037fbfb5499afad86ddb364
Tit Šober <tit.sober33@gmail.com>
2022-09-05 00:35:32 +0200

    fixing bugs on update_player.php in progress. SQL has to be fixed

--- backend/update_player.php
+++ backend/update_player.php
@@ -22,13 +22,13 @@ class ErrorCode{
 function upload_files($files_names){
     
     $files_uploaded = new ErrorCode(TRUE,"");
-    for($i = 0; $i < count($files_names);$i++){
+    
         $errors= array();
-        $file_name = str_replace(" ", "-",$_FILES[$files_names[$i]]['name']);
-        $file_size =$_FILES[$files_names[$i]]['size'];
-        $file_tmp =$_FILES[$files_names[$i]]['tmp_name'];
-        $file_type=$_FILES[$files_names[$i]]['type'];
-        $file_ext = strtolower(end(explode('.',$_FILES[$files_names[$i]]['name'])));
+        $file_name = str_replace(" ", "-",$_FILES[$files_names]['name']);
+        $file_size =$_FILES[$files_names]['size'];
+        $file_tmp =$_FILES[$files_names]['tmp_name'];
+        $file_type=$_FILES[$files_names]['type'];
+        $file_ext = strtolower(end(explode('.',$_FILES[$files_names]['name'])));
         
         $extensions= array("jpeg","jpg","png");
         
@@ -41,14 +41,14 @@ function upload_files($files_names){
         
      
         if(empty($errors)==true){
-        move_uploaded_file($file_tmp,"../images/teams/".$files_names[$i]."/".$file_name);
+        move_uploaded_file($file_tmp,"../images/".$files_names."/".$file_name);
         
         }else{
         
         return new ErrorCode(FALSE,$errors);
         }
     
-    }
+    
     return $files_uploaded;
 
 }
@@ -56,15 +56,20 @@ function upload_files($files_names){
 $files = array("front","leva_profilna","desna_profilna","lower_third","zmagovalna");
 $filesThatExist = array();
 
+
+
+$numOfElements = count($files);
 /* Checking if the file exists and if it does it is pushing it to the array and uploads it to the ftp server. */
-for($i = 0; $i < count($files); $i++){
+for($i = 0; $i < $numOfElements; $i++){
     if(file_exists($_FILES[$files[$i]]['tmp_name'])){
-        if($message = upload_files($files[$i])->get_flag()){
-            array_push($filesThatExist, $name);
+        echo "uploading ".$files[$i];
+        $message = upload_files($files[$i]);
+        if($message->get_flag()){
+            array_push($filesThatExist, $files[$i]);
 
         }
         else{
-            header("Location: ../admin/player.php?message=".$message->get_error_code());
+            header("Location: ../admin/manage_players.php?message=".$message->get_error_code());
 
         }
     }
@@ -82,27 +87,48 @@ $team = mysqli_real_escape_string($conn,$_POST['ekipa']);
 $eq = mysqli_real_escape_string($conn,$_POST['oprema']);
 $status = mysqli_real_escape_string($conn,$_POST['status']);
 $platform = mysqli_real_escape_string($conn,$_POST['platforma']);
-$gear = mysqli_real_escape_string($conn,$_POST['Gearbox']);
-$abs = mysqli_real_escape_string($conn,$_POST['ABS']);
-$rl = mysqli_real_escape_string($conn,$_POST['Racing']);
-$traction = mysqli_real_escape_string($conn,$_POST['Traction']);
+
 $id = mysqli_real_escape_string($conn,$_POST['id']);
+if(isset($_POST['Gearbox'])){
+    $gear = mysqli_real_escape_string($conn,$_POST['Gearbox']);
+ }else{
+    $gb = 0;
+ }
+ if(isset($_POST['ABS'])){
+    $abs = mysqli_real_escape_string($conn,$_POST['ABS']);
+ }else{
+    $abs = 0;
+ }
+ if(isset($_POST['Racing'])){
+    $rl = mysqli_real_escape_string($conn,$_POST['Racing']);
+ }else{
+    $rl = 0;
+ }
+ if(isset($_POST['Traction'])){
+    $traction = mysqli_real_escape_string($conn,$_POST['Traction']);
+ }else{
+    $traction = 0;
+ }
 
 
 $getDBFileNames = "SELECT front_photo fp,right_profile_photo rp,left_profile_photo lp,winner_photo wp,LT_photo ltp WHERE iddriver = $id;";
 
 if($result=mysqli_query($conn,$getDBFileNames)){
     while($row = mysqli_fetch_assoc($result)){
-        $sql = "UPDATE driver SET name = $name, lastname = $lastname, discord_username=$discord,platform=$platform,game_tag=$tag,steam_friend_code=$steam_code,driver_status=$status,date_of_birth=$date,equipment=$eq,driver_number=$number,Gearbox=$gear,ABS=$abs,Traction_control=$traction,Racing_line=$rl,front_photo = if(".file_exists($_FILES['front']['tmp_name']).",".$_FILES['front']['name'].",".$row['fp']."),left_profile_photo = if(".file_exists($_FILES['leva_profilna']['tmp_name']).",".$_FILES['leva_profilna']['name'].",".$row['lp']."), right_profile_photo = if(".file_exists($_FILES['desna_profilna']['tmp_name']).",".$_FILES['desna_profilna']['name'].",".$row['rp']."),winner_photo =if(".file_exists($_FILES['zmagovalna']['tmp_name']).",".$_FILES['zmagovalna']['name'].",".$row['wp']."),LT_photo = if(".file_exists($_FILES['lower_third']['tmp_name']).",".$_FILES['lower_third']['name'].",".$row['ltp'].") WHERE iddriver = $id;";
+        $sql = "UPDATE driver SET name = $name, lastname = $lastname, discord_username=$discord,platform=$platform,game_tag=$tag,steam_friend_code=$steam_code,driver_status=$status,date_of_birth=$date,equipment=$eq,driver_number=$number,Gearbox=$gear,ABS=$abs,Traction_control=$traction,Racing_line=$rl,front_photo = if(".echo (file_exists($_FILES['front']['tmp_name'])) ? '1' : '0'.",'".$_FILES['front']['name']."','".$row['fp']."'),left_profile_photo = if(".file_exists($_FILES['leva_profilna']['tmp_name']).",".$_FILES['leva_profilna']['name'].",".$row['lp']."), right_profile_photo = if(".file_exists($_FILES['desna_profilna']['tmp_name']).",".$_FILES['desna_profilna']['name'].",".$row['rp']."),winner_photo =if(".file_exists($_FILES['zmagovalna']['tmp_name']).",".$_FILES['zmagovalna']['name'].",".$row['wp']."),LT_photo = if(".file_exists($_FILES['lower_third']['tmp_name']).",".$_FILES['lower_third']['name'].",".$row['ltp'].") WHERE iddriver = $id;";
         if($result = mysqli_query($conn, $sql)){
-            header("Location: ../admin/player.php?message=success");
+            header("Location: ../admin/manage_players.php?message=success");
         }
-        else{header("Location: ../admin/player.php?message=error");}
+        else{header("Location: ../admin/manage_players.php?message=error");}
 
 
 
 
     } 
+}else{
+    echo "error uploading\n";
+    echo(file_exists($_FILES['front']['tmp_name']));
+
 }
 
 
